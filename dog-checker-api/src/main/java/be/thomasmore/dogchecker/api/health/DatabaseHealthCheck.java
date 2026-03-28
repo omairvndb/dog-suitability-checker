2a0b7f5826b6306f6fad680c185eaed354f06847
omair <o.vandenbroucke@outlook.com>
2026-03-28 14:24:50 +0100

    feat(api): rename generic api health check to database health check

--- dog-checker-api/src/main/java/be/thomasmore/dogchecker/api/health/DatabaseHealthCheck.java
+++ dog-checker-api/src/main/java/be/thomasmore/dogchecker/api/health/DatabaseHealthCheck.java
@@ -8,13 +8,11 @@ import org.eclipse.microprofile.health.Liveness;
 
 @Liveness
 @ApplicationScoped
-public class ApiHealthCheck implements HealthCheck {
+public class DatabaseHealthCheck implements HealthCheck {
 
     @Override
     public HealthCheckResponse call() {
         try {
-            // Check if we can access the database by using a simple query like counting 
-            // the number of DogWeatherRequest entries
             DogWeatherRequest.count();
             return HealthCheckResponse.up("Database connection is healthy");
         } catch (Exception e) {
